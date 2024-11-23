687c8bb370b1a3de3d4e7ac8bffa1534e6af494f
dylan <dylanwanjiku43@gmail.com>
2024-11-24 02:44:41 +0300

    new files

---
+++ Lesson9.py
@@ -0,0 +1,16 @@
+#loops
+
+while True:
+    print('Hello, loops')
+    break  #stop
+scores = [90, 89, 45, 34, 23, 23, 23, 23, 23, 23, 21, 14, 56, 45, 23, 67, 88, 89, 85]
+#for each loop
+for score in scores:
+    if score >= 20 and score <= 90 and score % 2 == 0:
+        print(score)
+
+
+
+
+
+        
---
+++ lesson11.py
@@ -0,0 +1,46 @@
+#functions
+from datetime import date
+
+
+def cal_area_triangle(b, h):
+    area = 0.5 * b * h
+    print(area)
+
+
+def cal_area_circle(radius):
+    area = 22/7 * radius * radius
+    area=round(area,2)
+    print("Area of circle is", area,"cm^2")
+
+def print_todays_date():
+    todays= date.today()
+    print("Today's date is", todays)
+
+def add(*args):
+    total=0
+    for num in args:
+        total += num
+        print("tOtal is ", total)
+
+
+def sayHi(name, age=21):
+    print("Hello " + name + ", I am " + str(age) + " years old.")
+
+sayHi('Mary')
+sayHi('kevo',20)
+
+
+#use a func==calling
+cal_area_triangle(8, 13)
+cal_area_triangle(16, 40)
+
+triangles = [[8, 9], [9, 7], [4, 12], [3, 6], [13, 6]]
+
+for triangle in triangles:
+    cal_area_triangle(triangle[0], triangle[1])
+
+cal_area_circle(28.73653)
+print_todays_date()
+
+add(1,2)
+add(12,22,46,78,56,34,13,55)
